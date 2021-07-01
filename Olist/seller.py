b8eb58a4c72f2425d52836e7e2841edaa516be12
Corentin Garet <garetcorentin@gmail.com>
2021-07-01 19:09:46 +0200

    sellers analysis

--- Olist/seller.py
+++ Olist/seller.py
@@ -7,7 +7,7 @@ from Olist.order import Order
 class Seller:
 
     def __init__(self):
-        # Import data only once
+        # Import only data once
         olist = Olist()
         self.data = olist.get_data()
         self.matching_table = olist.get_matching_table()
@@ -18,12 +18,9 @@ class Seller:
         Returns a DataFrame with:
         'seller_id', 'seller_city', 'seller_state'
         """
-        # Make a copy before using inplace=True so as to avoid modifying
-        # self.data
-        sellers = self.data['sellers'].copy()
+        sellers = self.data['sellers'].copy() # Make a copy before using inplace=True so as to avoid modifying self.data
         sellers.drop('seller_zip_code_prefix', axis=1, inplace=True)
-        # There are multiple rows per seller
-        sellers.drop_duplicates(inplace=True)
+        sellers.drop_duplicates(inplace=True) # There is multiple rows per seller
         return sellers
 
     def get_seller_delay_wait_time(self):
@@ -38,28 +35,25 @@ class Seller:
         ship = order_items.merge(orders, on='order_id')
 
         # Handle datetime
-        ship.loc[:, 'shipping_limit_date'] = pd.to_datetime(
-            ship['shipping_limit_date'])
-        ship.loc[:, 'order_delivered_carrier_date'] = pd.to_datetime(
-            ship['order_delivered_carrier_date'])
-        ship.loc[:, 'order_delivered_customer_date'] = pd.to_datetime(
-            ship['order_delivered_customer_date'])
-        ship.loc[:, 'order_purchase_timestamp'] = pd.to_datetime(
-            ship['order_purchase_timestamp'])
+        ship.loc[:, 'shipping_limit_date'] = pd.to_datetime(ship['shipping_limit_date'])
+        ship.loc[:, 'order_delivered_carrier_date'] = pd.to_datetime(ship['order_delivered_carrier_date'])
+        ship.loc[:, 'order_delivered_customer_date'] = pd.to_datetime(ship['order_delivered_customer_date'])
+        ship.loc[:, 'order_purchase_timestamp'] = pd.to_datetime(ship['order_purchase_timestamp'])
 
         # Compute delay and wait_time
-        def delay_to_logistic_partner(df):
+        def delay_to_logistic_partner(d):
             days = np.mean(
-                (df.shipping_limit_date - df.order_delivered_carrier_date)
-                / np.timedelta64(24, 'h'))
+                (d.shipping_limit_date - d.order_delivered_carrier_date)/np.timedelta64(24, 'h')
+                )
             if days < 0:
                 return abs(days)
-            return 0
+            else:
+                return 0
 
-        def order_wait_time(df):
+        def order_wait_time(d):
             days = np.mean(
-                (df.order_delivered_customer_date - df.order_purchase_timestamp)
-                / np.timedelta64(24, 'h'))
+                (d.order_delivered_customer_date - d.order_purchase_timestamp)/np.timedelta64(24, 'h')
+                )
             return days
 
         delay = ship.groupby('seller_id')\
@@ -72,53 +66,61 @@ class Seller:
                    .reset_index()
         wait.columns = ['seller_id', 'wait_time']
 
-        order_wait_time_df = delay.merge(wait, on='seller_id')
+        df = delay.merge(wait, on='seller_id')
 
-        return order_wait_time_df
+        return df
 
     def get_active_dates(self):
         """
-        Returns a DataFrame with: 'seller_id', 'date_first_sale',
-        'date_last_sale'
+        Returns a DataFrame with: 'seller_id', 'date_first_sale', 'date_last_sale'
         """
+
         orders = self.data['orders'][['order_id', 'order_approved_at']].copy()
 
-        # create two new columns with a view to aggregate
-        orders.loc[:, 'date_first_sale'] = pd.to_datetime(
-            orders['order_approved_at'])
+        # create two new columns in view of aggregating
+        orders.loc[:,'date_first_sale'] = pd.to_datetime(orders['order_approved_at'])
         orders['date_last_sale'] = orders['date_first_sale']
 
-        return orders.merge(
-            self.matching_table[['seller_id', 'order_id']], on="order_id")\
+        df = orders.merge(self.matching_table[['seller_id', 'order_id']], on="order_id")\
             .groupby('seller_id')\
             .agg({
-                "date_first_sale": min,
-                "date_last_sale": max
-            })
+            "date_first_sale": min,
+            "date_last_sale": max
+        })
+
+        df['months_on_olist'] = round((df['date_last_sale']-df['date_first_sale']) / np.timedelta64(1, 'M'))
+
+        return df
 
     def get_review_score(self):
         """
         Returns a DataFrame with:
-        'seller_id', 'share_of_five_stars', 'share_of_one_stars', 'review_score'
+        'seller_id', 'share_of_five_stars', 'share_of_one_stars', 'review_score', 'cost_of_reviews'
         """
         matching_table = self.matching_table
         orders_reviews = self.order.get_review_score()
 
-        # Since the same seller can appear multiple times in the same order,
-        # create a (seller <> order) matching table
+        # Since same seller can appear multiple times in the same order, create a (seller <> order) matching table
+        matching_table = matching_table[['order_id', 'seller_id']].drop_duplicates()
+        df = matching_table.merge(orders_reviews, on='order_id')
 
-        matching_table = matching_table[['order_id', 'seller_id']]\
-            .drop_duplicates()
-        reviews_df = matching_table.merge(orders_reviews, on='order_id')
-        reviews_df = reviews_df.groupby(
-            'seller_id', as_index=False).agg({'dim_is_one_star': 'mean',
-                                              'dim_is_five_star': 'mean',
-                                              'review_score': 'mean'})
-        # Rename columns
-        reviews_df.columns = ['seller_id', 'share_one_stars',
-                              'share_of_five_stars', 'review_score']
+        df['cost_of_reviews'] = df.review_score.map({
+            1: 100,
+            2: 50,
+            3: 40,
+            4: 0,
+            5: 0
+        })
 
-        return reviews_df
+        df = df.groupby('seller_id',
+                        as_index=False).agg({'dim_is_one_star': 'mean',
+                                             'dim_is_five_star': 'mean',
+                                             'review_score': 'mean',
+                                             'cost_of_reviews': 'sum'})
+        df.columns = ['seller_id', 'share_of_one_stars',
+                      'share_of_five_stars', 'review_score', 'cost_of_reviews']
+
+        return df
 
     def get_quantity(self):
         """
@@ -128,11 +130,11 @@ class Seller:
         matching_table = self.matching_table
 
         n_orders = matching_table.groupby('seller_id')['order_id']\
-            .nunique().reset_index()
+            .nunique()\
+            .reset_index()
         n_orders.columns = ['seller_id', 'n_orders']
 
-        quantity = matching_table.groupby('seller_id', as_index=False)\
-            .agg({'order_id': 'count'})
+        quantity = matching_table.groupby('seller_id', as_index=False).agg({'order_id': 'count'})
         quantity.columns = ['seller_id', 'quantity']
 
         result = n_orders.merge(quantity, on='seller_id')
@@ -149,13 +151,13 @@ class Seller:
             .sum()\
             .rename(columns={'price': 'sales'})
 
+
     def get_training_data(self):
         """
         Returns a DataFrame with:
         'seller_id', 'seller_state', 'seller_city', 'delay_to_carrier',
         'wait_time', 'share_of_five_stars', 'share_of_one_stars',
-        'seller_review_score', 'n_orders', 'quantity,' 'date_first_sale',
-        'date_last_sale', 'sales'
+        'seller_review_score', 'n_orders', 'quantity,' 'date_first_sale', 'date_last_sale', 'sales'
         """
 
         training_set =\
@@ -171,44 +173,18 @@ class Seller:
                ).merge(
                 self.get_sales(), on='seller_id'
                )
-
-        return training_set
-
-    def get_ceo_request_data(self):
         
-        sellers = self.get_training_data()
-
-        # Get revenue per seller
-
-        sellers['olist_age'] = round((sellers['date_last_sale'] - sellers['date_first_sale']) / np.timedelta64(1, 'M'))
-        sellers['total_revenue'] = sellers['olist_age'] * 80 + sellers['sales'] * 0.1
-
-        # Get cost per seller
-
-        # 1 Def cost function
-        def cost_review(x):
-            if x == 1:
-                return 100
-            if x == 2:
-                return 50
-            if x == 3:
-                return 40
-            return 0
-
-        # 2 Get matching table
-        table = self.matching_table
-        order_reviews = self.data['order_reviews']
-        new_table = pd.merge(table[['seller_id', 'order_id']], order_reviews[['review_score', 'order_id']], how="inner", on="order_id")
-        new_table['review_cost'] = new_table['review_score'].apply(func=cost_review)
-        review_cost_seller = new_table.groupby('seller_id').sum()[['review_cost']]
-
-        # 3 Création de new_sellers
-        new_sellers = pd.merge(sellers, review_cost_seller, how="inner", on="seller_id")
-        #new_sellers['it_cost'] = round(500_000 / sum(new_sellers['n_orders']**0.5) * new_sellers['n_orders']**0.5)
-        #new_sellers['total_cost'] = new_sellers['it_cost'] + new_sellers['review_cost']
-
-        # Get profit
-
-        new_sellers['profit'] = new_sellers['total_revenue'] - new_sellers['review_cost']
-
-        return new_sellers
+        # Add seller economics (revenues, profits)
+        olist_monthly_fee = 80
+        olist_sales_cut = 0.1
+        training_set['revenues'] = training_set['months_on_olist'] * olist_monthly_fee\
+            + olist_sales_cut * training_set['sales']
+        training_set['profits'] = training_set['revenues'] - training_set['cost_of_reviews']
+        # It cost
+        total_it_cost = 500_000
+        
+        k = 500_000 / sum(training_set['n_orders']**0.5)
+        training_set['it_cost'] = k * training_set['n_orders']**0.5
+        round(training_set['it_cost'].sum())
+        
+        return training_set
